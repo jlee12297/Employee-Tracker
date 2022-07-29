f16159c1610790c7ced0a6d189888f8b5acc14b4
Josh <jlee12297@gmail.com>
2022-07-28 20:27:52 -0700

    added helper and middleware files, a server.js to work with, set up necessary basic imports and port in server js

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const { clog } = require('./middleware/clog');
+const api = require('./routes/index.js');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
